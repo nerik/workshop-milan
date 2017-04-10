e72f600671f2cb1414a10c94d262b1b295cda32d
nerik <erik.escoffier@gmail.com>
2017-04-10 21:29:40 +0200

    slide those slides

--- prez/index.js
+++ prez/index.js
@@ -19,11 +19,37 @@ Promise.all(['./slides.json','./decks.json'].map(url => fetch(url)
       return slide;
     });
 
-    console.log(milanSlides);
+    // console.log(milanSlides);
 
     const template = _.template(document.getElementById('tpl-slides').innerText);
 
     document.getElementById('slides').innerHTML = template({
       slides: milanSlides
     });
+
+    let currentSlideIndex;
+    const slidesSections = document.querySelectorAll('section');
+    const numSlides = slidesSections.length;
+
+    const gotoSlide = (slideIndex) => {
+      currentSlideIndex = Math.min(numSlides - 1, Math.max(0, slideIndex));
+      document.querySelectorAll('section').forEach((slide, index) => {
+        if (index === slideIndex) {
+          slide.classList.add('-current');
+        } else {
+          slide.classList.remove('-current');
+        }
+      });
+      window.location.hash = currentSlideIndex;
+    };
+
+    document.addEventListener('keydown', e => {
+      if (e.keyCode === 39) { // right
+        gotoSlide(currentSlideIndex +1);
+      } else if (e.keyCode === 37) { // left
+        gotoSlide(currentSlideIndex - 1);
+      }
+    });
+
+    gotoSlide((window.location.hash === '') ? 0 : parseInt(window.location.hash.substr(1)));
   });
